fe4560c8c9bcadc4abbf678b60ff76a87fd36a1a
lxr <liangxuanran@baidu.com>
2018-05-18 16:56:53 +0800

    my first push cd python_spider2/python_spider/

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 ################################################################################
 #
-# Copyright (c) 2016 Baidu.com, Inc. All Rights Reserved
+# Copyright (c) 2016 Baidu.com, Inc. All Rights Reserved [conflict try!!]
 #
 ################################################################################
 from src.spider_conf import SpiderConf
